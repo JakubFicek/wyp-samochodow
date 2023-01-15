7431fc9076f86bae81f7fd608ad40d1abc783c62
Jakub Ficek <jakubficek138@gmail.com>
2023-01-15 20:02:01 +0100

    Zmiana z localhost 3000 na 5000, ze wzgledu na kolizje z reactem

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
   );
   app.useGlobalPipes(new ValidationPipe({forbidUnknownValues: false}));
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(5000);
 }
 bootstrap();
